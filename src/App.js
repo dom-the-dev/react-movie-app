7d060fe08ab2732142d0f62227c994692f801056
dom-the-dev <hi@domthedev.com>
2020-12-17 10:19:41 +0100

    Add search function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import MovieCard from './components/MovieCard';
 
-const TRENDING_VIDEOS = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_MOVIE_API}`
+const TRENDING_VIDEOS = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_MOVIE_API}`;
+const SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API}`
 
 function App() {
 
     const [movies, setMovies] = useState(null);
+    const [searchQuery, setSearchQuery] = useState(null);
 
     useEffect(() => {
 
@@ -29,9 +31,29 @@ function App() {
         })
     }
 
+    const searchMovies = async (event, searchQuery) => {
+        event.preventDefault();
+        const query = searchQuery.replaceAll(' ', '+');
+
+        try {
+            const searchResult = await axios.get(`${SEARCH}&query=${query}`)
+            setMovies(searchResult.data.results);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className="App">
             <h1 className="app-title">React Movie App</h1>
+
+            <div className="search">
+                <form onSubmit={(e) => searchMovies(e, searchQuery)}>
+                    <input className={"search-input"} onChange={e => setSearchQuery(e.target.value)} type="text" placeholder={"Search"} />
+                    <button className={"search-button"} type="submit">Go</button>
+                </form>
+            </div>
+
             <div className="movies">
                 {movies && movies.length ? renderMovies(movies) : 'no movies available'}
             </div>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,14 +3,16 @@ import React from 'react'
 const IMAGE_PATH = 'https://image.tmdb.org/t/p/w200'
 
 function MovieCard({ movie }) {
-    console.log(movie);
     return (
         <div className={'movie-card'}>
-            <img src={IMAGE_PATH + movie.poster_path} alt={movie.title} />
+            {movie.poster_path ?
+                <img src={IMAGE_PATH + movie.poster_path} alt={movie.title} />
+                : <div className="no-image">no image available</div>
+            }
             <div className="movie-card__info">
-                <span className="movie-card__title">
+                <h4 className="movie-card__title">
                     {movie.title ? movie.title : movie.name}
-                </span>
+                </h4>
                 <span className="movie-card__voting">
                     {movie.vote_average}
                 </span>
